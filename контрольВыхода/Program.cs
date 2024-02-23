56d286229496f48281bcf5d61c84e9eff4099d09
79612 <79612@DESKTOP-O1P5TC5>
2024-02-23 22:45:17 +0300

    Добавьте файлы проекта.

---
+++ контрольВыхода/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace контрольВыхода
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Для выхода введите: exit");
+                string wordExit = Console.ReadLine();
+
+                if(wordExit == "exit")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
